0240e3e7136e363d7b221699206048a8ba7500b3
Yuunhye <pyl66600@gmail.com>
2024-01-03 05:45:59 +0900

    [PGS] 행렬과 연산 - 효율성 통과

--- yoonhye/KAKAO/2022_TECH_INTERNSHIP/[PGS] 행렬과 연산.py
+++ yoonhye/KAKAO/2022_TECH_INTERNSHIP/[PGS] 행렬과 연산.py
@@ -1,26 +1,54 @@
 from collections import deque
 
+
 def solution(rc, operations):
     M = len(rc[0])  # 열의 개수
-    N = len(rc)  # 행의 개수
-    start = 0
-    end = N - 1
 
-    rc_deque = deque([])
-    for arr in rc:
-        rc_deque.append(deque(arr))
+    # 예외처리 (열이 2개만 있는 경우 - 런타임 에러 방지)
+    if M == 2:
+        first_col = deque()
+        last_col = deque()
+        for arr in rc:
+            first_col.append(arr[0])
+            last_col.append(arr[1])
 
-    for op in operations:
-        if op == "Rotate":
-            # 첫 열 : popleft해서 한 행 위에 appendleft
-            # 마지막 열 : pop해서 한 행 밑에 append
-            for i in range(1, N):
-                rc_deque[i - 1].appendleft(rc_deque[i].popleft())
-                rc_deque[N - i].append(rc_deque[N - i - 1].pop())
-        else:
-            rc_deque.appendleft(rc_deque.pop())
+        for op in operations:
+            if op == "Rotate":
+                last_col.appendleft(first_col.popleft())
+                first_col.append(last_col.pop())
+            else:
+                first_col.appendleft(first_col.pop())
+                last_col.appendleft(last_col.pop())
+        result = []
+        for fc, lc in zip(first_col, last_col):
+            result.append([fc, lc])
 
-    result = []
-    for deq in rc_deque:
-        result.append(list(deq))
+    else:
+        first_col = deque()
+        last_col = deque()
+        rc_deque = deque()
+        for arr in rc:
+            arr = deque(arr)
+            first_col.append(arr.popleft())
+            last_col.append(arr.pop())
+            rc_deque.append(arr)
+
+        for op in operations:
+            if op == "Rotate":
+                last_col.appendleft(rc_deque[0].pop())
+                rc_deque[-1].append(last_col.pop())
+                first_col.append(rc_deque[-1].popleft())
+                rc_deque[0].appendleft(first_col.popleft())
+            else:
+                last_col.appendleft(last_col.pop())
+                first_col.appendleft(first_col.pop())
+                rc_deque.appendleft(rc_deque.pop())
+
+        result = []
+        for fc, arr, lc in zip(first_col, rc_deque, last_col):
+            row = []
+            row.append(fc)
+            row.extend(arr)
+            row.append(lc)
+            result.append(row)
     return result
