bfb0aa6f261464437926fbbdd6ca76850983540a
Yuunhye <pyl66600@gmail.com>
2024-01-03 05:17:00 +0900

    [PGS] 행렬과 연산 - 효율성 5/9 해결

--- yoonhye/KAKAO/2022_TECH_INTERNSHIP/[PGS] 행렬과 연산.py
+++ yoonhye/KAKAO/2022_TECH_INTERNSHIP/[PGS] 행렬과 연산.py
@@ -1,37 +1,26 @@
 from collections import deque
 
-def Rotate(rc, N, M):
-    row = [rc[0][:], rc[-1][:]]  # 첫 행, 마지막 행
-
-    column = [[], []]  # 첫 열, 마지막 열
-    for k in range(N):
-        column[0].append(rc[k][0])
-        column[1].append(rc[k][-1])
-
-    rc[0][1] = row[0][0]
-    rc[-1][M - 2] = row[1][M - 1]
-    for i in range(1, M - 1):
-        rc[0][i + 1] = row[0][i]
-        rc[-1][i - 1] = row[1][i]
-
-    rc[1][-1] = row[0][-1]
-    rc[N - 2][0] = row[1][0]
-    for j in range(1, N - 1):
-        rc[j + 1][-1] = column[1][j]
-        rc[j - 1][0] = column[0][j]
-    return rc
-
-
 def solution(rc, operations):
     M = len(rc[0])  # 열의 개수
     N = len(rc)  # 행의 개수
     start = 0
     end = N - 1
-    rc = deque(rc)
+
+    rc_deque = deque([])
+    for arr in rc:
+        rc_deque.append(deque(arr))
+
     for op in operations:
         if op == "Rotate":
-            rc = Rotate(rc, N, M)
+            # 첫 열 : popleft해서 한 행 위에 appendleft
+            # 마지막 열 : pop해서 한 행 밑에 append
+            for i in range(1, N):
+                rc_deque[i - 1].appendleft(rc_deque[i].popleft())
+                rc_deque[N - i].append(rc_deque[N - i - 1].pop())
         else:
-            rc.appendleft(rc.pop())
+            rc_deque.appendleft(rc_deque.pop())
 
-    return list(rc)
+    result = []
+    for deq in rc_deque:
+        result.append(list(deq))
+    return result
