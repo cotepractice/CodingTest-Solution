9833c19d553072d0acdf2d4479cccefe085277b2
seoyeon0201 <psy0201123@gmail.com>
2024-01-20 21:25:47 +0900

    [BOJ] 섬의 개수

--- seoyeon/백준/그래프/섬의 개수.py
+++ seoyeon/백준/그래프/섬의 개수.py
@@ -1,10 +1,10 @@
 #백준 #4963 섬의 개수
 #그래프 문제
-#21:10-
+#21:10-21:24
 
 from collections import deque
 
-#하상좌우대각선4개
+#하상좌우+대각선4개
 dx = [0,0,-1,1,-1,1,-1,1]
 dy = [-1,1,0,0,1,1,-1,-1]
 
@@ -29,14 +29,17 @@ def bfs(x,y):
 while True:
     w,h = map(int, input().split())
 
+    #종결조건
     if (w==0 and h==0):
         break
 
+    #graph 정의
     graph = [[0 for _ in range(w)] for _ in range(h)]
 
     for i in range(h):
         graph[i] = list(map(int, input().split()))
     
+    #제시한 문제 해결
     cnt = 0
     for i in range(h):
         for j in range(w):
