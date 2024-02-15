f6ca09475d8cb70302d9b916e383dd717db22281
seoyeon0201 <psy0201123@gmail.com>
2024-02-15 17:44:14 +0900

    [BOJ] 경사로

--- seoyeon/백준/삼성 SW 역량 테스트 기출/경사로.py
+++ seoyeon/백준/삼성 SW 역량 테스트 기출/경사로.py
@@ -2,93 +2,54 @@
 #삼성 SW 역량 테스트 기출
 #14:54-15:57
 #16:05-16:16
+#16:43-17:43
 
 
 N, L = map(int, input().split())
 
 lst = [[0 for i in range(N)] for i in range(N)]
-dp = [[True for i in range(N)] for i in range(N)]
+dp = [[True for i in range(N)] for i in range(N)]   #경사로가 놓이면 False
 
 for i in range(N):
     lst[i] = list(map(int, input().split()))
 
-cnt = 0 
-
-#가로 확인
-for i in range(N):
-    count = 0 #같은 숫자가 반복되는 횟수
-    check = True
-    idx = -1
-    j = 1
-    target = lst[i][0]
-    while (j<=N-1 and check == True):
-        print("target",target,lst[i][j])
-        #뒤(현재,j)가 target보다 작은 경우
-        if lst[i][j]+1 == target:
-            # print("1", check)
-            idx = j
-            count = 1
-            target = lst[i][j]
-            print("1", check)
-        #뒤(현재,j)가 target보다 큰 경우
-        elif lst[i][j]-1 == target:
-            # print("2")
-            if (count >= L):
-                #dp가 False인 경우 체크
-                for k in range(L):
-                    if (dp[i][j-k-1] == False):
-                        check = False
-                if (check == True):
-                    for k in range(L):
-                        dp[i][j-k-1] = False
-            else:
-                check = False
-            print("2", check)
-
-        #앞과 뒤가 같은 경우
-        else:
-            # print("3")
-            count += 1
-            # if (idx != -1):
-            #     if (count>=L):
-            #         for k in range(L):
-            #             if (dp[i][idx-k-1] == False):
-            #                 check = False
-            #                 idx = -1
-            #         if (check == True):
-            #             for k in range(L):
-            #                 dp[i][idx-k-1] = False
-            #                 idx = -1
-            print("3", check)
-
-        if (j==N-1):
-            if (idx != -1):
-                if (count>=L):
-                    for k in range(L):
-                        if (dp[i][idx-k-1] == False):
-                            check = False
-                            idx = -1
-                    if (check == True):
-                        for k in range(L):
-                            dp[i][idx-k-1] = False
-                            idx = -1
-                else:
-                    check = False
-
-        # print("j",j,check)
-        j += 1
-    print("check:",check)
-    #뒤(현재)가 앞보다 작은 경우, 그 뒤 처리
-    if (idx!=-1 and count >= L and check == True):
-        for k in range(L):
-            if (dp[i][idx-k-1] == False):
-                check = False
-        if (check == True):
+def pos(now):
+    for i in range(1,N):    #1부터 시작
+        #2 이상 차이나는 경우
+        if abs(now[i]-now[i-1]) > 1:
+            return False
+        #뒤의 index가 더 큰 경우 => 경사로를 앞에 둬야함
+        elif (now[i] > now[i-1]):
             for k in range(L):
-                dp[i][idx-k-1] = False
+                #앞의 index인 i-1과 비교
+                #1)같은 높이가 아니거나 2)N보다 범위가 같거나 커지거나 3)이미 경사로가 있는 경우 제외
+                if (i-k-1 < 0) or (now[i-1] != now[i-k-1]) or (dp[i-k-1] == True):
+                    return False
+                if (now[i-1] == now[i-k-1]):
+                    dp[i-k-1] = True
 
-    if (j == N and check == True):
+        #앞의 index가 더 큰 경우 => 경사로를 뒤에 둬야함
+        elif (now[i] < now[i-1]):
+            for k in range(L):
+                #1)같은 높이가 아니거나 2)N보다 범위가 같거나 커지거나 3)이미 경사로가 있는 경우 제외
+                #*index 확인을 가장 앞에 둬야함
+                if (i+k >= N) or (now[i] != now[i+k]) or (dp[i+k] == True):
+                    return False
+                if (now[i] == now[i+k]):
+                    dp[i+k] = True
+    return True
+
+cnt = 0
+
+for i in range(N):
+    dp = [False for _ in range(N)]
+    if pos(lst[i]): #True를 반환할 경우 cnt 증가
         cnt += 1
 
-print("cnt:",cnt)
-#세로 확인
+for i in range(N):
+    dp = [False for _ in range(N)]
+    #*lst의 세로를 새로운 list 형태로 만들어야함
+    if pos([lst[j][i] for j in range(N)]):
+        cnt += 1
+
+print(cnt)
