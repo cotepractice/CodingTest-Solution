7238384efd024791b9a25c34c65a42adbf708965
seoyeon0201 <psy0201123@gmail.com>
2024-02-22 20:51:44 +0900

    [BOJ] 마법사 상어와 비바라기-시간초과 해결

--- seoyeon/백준/삼성 SW 역량 테스트 기출/마법사 상어와 비바라기.py
+++ seoyeon/백준/삼성 SW 역량 테스트 기출/마법사 상어와 비바라기.py
@@ -1,5 +1,7 @@
 #백준 #21610 마법사 상어와 비바라기
 #삼성 SW 역량 테스트 기출
+#시간초과4만 해결하면 시간초과 해결
+
 from collections import deque
 
 n, m = map(int, input().split())
@@ -29,36 +31,39 @@ while move:
     #1. 모든 구름이 d 방향으로 s만큼 이동
     d,s = move.popleft()
     cloudMoved = []
+    visited = [[False for _ in range(n)] for _ in range(n)]
 
     for k in range(len(cloudLst1)):
         x,y = cloudLst1[k]
-        nx = x + s*dx[d-1]
-        ny = y + s*dy[d-1]
+        nx = (x + s*dx[d-1]) % n   #시간초과 해결1. while문으로 해결하던 것을 나머지를 나타내는 %를 이용해 해결
+        ny = (y + s*dy[d-1]) % n
 
-        while True:
-            if nx<0:
-                nx += n
+        # while True:   #시간초과 해결1 원본은 nx,ny에 "%n" 없고 주석 처리된 코드 존재
+        #     if nx<0:
+        #         nx += n
                 
-            elif nx>n-1: 
-                nx -= n
+        #     elif nx>n-1: 
+        #         nx -= n
                 
-            elif 0<=nx<n:
-                    break
-        while True:
-            if ny<0:
-                ny += n
-            elif ny>n-1:
-                ny -= n
-            elif 0<=ny<n:
-                break
+        #     elif 0<=nx<n:
+        #             break
+        # while True:
+        #     if ny<0:
+        #         ny += n
+        #     elif ny>n-1:
+        #         ny -= n
+        #     elif 0<=ny<n:
+        #         break
 
+        buckets[nx][ny] += 1    #시간초과 해결2. 아래에서 별도의 for문으로 진행하던 걸 하나의 for문 안에 넣음
+        visited[nx][ny] = True
         cloudMoved.append((nx,ny))
 
 
-    #2. 각 구름에 비가 내리고 사라짐
-    for k in range(len(cloudMoved)):
-        x, y = cloudMoved[k]
-        buckets[x][y] += 1
+    # #2. 각 구름에 비가 내리고 사라짐 => 시간초과 해결2는 원본 코드 한 줄 없고 주석 처리된 코드 존재
+    # for k in range(len(cloudMoved)):
+    #     x, y = cloudMoved[k]
+    #     buckets[x][y] += 1
 
     #3. 대각선 개수 구해서 bucket에 추가
     for k in range(len(cloudMoved)):
@@ -78,15 +83,22 @@ while move:
     for i in range(n):
         for j in range(n):
             if buckets[i][j] >= 2:
-                if (i,j) not in cloudMoved:
+                # if (i,j) not in cloudMoved:   #시간초과 해결4. in 문법은 시간 복잡도가 높으므로 visited로 True False에 따라 처리
+                if visited[i][j] == False:
                     cloudLst2.append((i,j))
                     buckets[i][j] -= 2
 
-    if (move == deque([])):
-        sum = 0
-        for i in range(n):
-            for j in range(n):
-                sum += buckets[i][j]
-        print(sum)
-        continue
+    # if (move == deque([])):   #시간초과 해결3. if문을 while문 밖으로 뺌
+    #     sum = 0
+    #     for i in range(n):
+    #         for j in range(n):
+    #             sum += buckets[i][j]
+    #     print(sum)
+    #     continue
     cloudLst1 = cloudLst2
+
+sum = 0
+for i in range(n):
+    for j in range(n):
+        sum += buckets[i][j]
+print(sum)
