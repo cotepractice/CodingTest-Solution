00eba13ab8ce1d05d347920895ca175f898ba204
seoyeon0201 <psy0201123@gmail.com>
2024-02-22 20:16:43 +0900

    [BOJ] 마법사 상어와 비바라기-시간초과

--- seoyeon/백준/삼성 SW 역량 테스트 기출/마법사 상어와 비바라기.py
+++ seoyeon/백준/삼성 SW 역량 테스트 기출/마법사 상어와 비바라기.py
@@ -4,10 +4,7 @@ from collections import deque
 
 n, m = map(int, input().split())
 buckets = [[0 for _ in range(n)] for _ in range(n)]
-clouds = [[False for _ in range(n)] for _ in range(n)]
 move = deque([])
-
-print("cloud",clouds)
 
 for i in range(n):
     buckets[i] = list(map(int, input().split()))
@@ -25,46 +22,71 @@ rx = [-1,-1,1,1]
 ry = [-1,1,-1,1]
 
 #(n-2,0),(n-2,1),(n-1,0),(n-1,1),에 구름 존재
-cloudPos = deque([(n-2,0),(n-2,1),(n-1,0),(n-1,1)])
+cloudLst1 = [(n-2,0),(n-2,1),(n-1,0),(n-1,1)]
 
 #main
-#1. 모든 구름이 d 방향으로 s만큼 이동
-d,s = move.popleft()
-for k in range(len(cloudPos)):
-    x,y = cloudPos.popleft()
-    nx = x + s*dx[d-1]
-    ny = y + s*dy[d-1]
+while move:
+    #1. 모든 구름이 d 방향으로 s만큼 이동
+    d,s = move.popleft()
+    cloudMoved = []
 
-    if nx<0:
+    for k in range(len(cloudLst1)):
+        x,y = cloudLst1[k]
+        nx = x + s*dx[d-1]
+        ny = y + s*dy[d-1]
+
         while True:
-            nx += n
-            if 0<=nx<n:
-                break
-    elif nx>n-1:
-        while True: 
-            nx -= n
-            if 0<=nx<n:
-                break
-    elif ny<0:
+            if nx<0:
+                nx += n
+                
+            elif nx>n-1: 
+                nx -= n
+                
+            elif 0<=nx<n:
+                    break
         while True:
-            ny += n
-            if 0<=ny<n:
+            if ny<0:
+                ny += n
+            elif ny>n-1:
+                ny -= n
+            elif 0<=ny<n:
                 break
-    elif ny>n-1:
-        while True:
-            y -= n
-            if 0<=y<n:
-                break
-    
-    cloudPos.append((nx,ny))
 
-#2. 각 구름에 비가 내리고 사라짐
-while cloudPos:
-    x, y = cloudPos.popleft()
-    cloudPos.append((x,y))
-    buckets[x][y] += 1
+        cloudMoved.append((nx,ny))
 
-#3. 
 
-print("clouds",clouds)
-print("buckets",buckets)
+    #2. 각 구름에 비가 내리고 사라짐
+    for k in range(len(cloudMoved)):
+        x, y = cloudMoved[k]
+        buckets[x][y] += 1
+
+    #3. 대각선 개수 구해서 bucket에 추가
+    for k in range(len(cloudMoved)):
+        x, y = cloudMoved[k]
+        cnt = 0
+        for k in range(4):
+            nx = x+rx[k]
+            ny = y+ry[k]
+            
+            if 0<=nx<n and 0<=ny<n:
+                if buckets[nx][ny] > 0:
+                    cnt += 1
+        buckets[x][y] += cnt
+
+    #4. 물의 양 2 이상인 칸에 구름 생김 => 이때 물의 양이 2만큼 줄어듬
+    cloudLst2 = []
+    for i in range(n):
+        for j in range(n):
+            if buckets[i][j] >= 2:
+                if (i,j) not in cloudMoved:
+                    cloudLst2.append((i,j))
+                    buckets[i][j] -= 2
+
+    if (move == deque([])):
+        sum = 0
+        for i in range(n):
+            for j in range(n):
+                sum += buckets[i][j]
+        print(sum)
+        continue
+    cloudLst1 = cloudLst2
