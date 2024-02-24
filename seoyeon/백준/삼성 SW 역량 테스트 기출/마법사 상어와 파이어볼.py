cc6564a83a80fac66b6c5b53a108097ecb7040f9
seoyeon0201 <psy0201123@gmail.com>
2024-02-24 22:41:17 +0900

    [BOJ] 마법사 상어와 파이어볼-테스트케이스 모두 성공

--- seoyeon/백준/삼성 SW 역량 테스트 기출/마법사 상어와 파이어볼.py
+++ seoyeon/백준/삼성 SW 역량 테스트 기출/마법사 상어와 파이어볼.py
@@ -14,6 +14,7 @@ dx = [-1,-1,0,1,1,1,0,-1]
 dy = [0,1,1,1,0,-1,-1,-1]
 
 for _ in range(K):
+    #print("fireballs",fireballs)
     #1. 모든 파이어볼이 자신의 방향과 속력대로 이동
     newFireballs = []   #이동한 파이어볼 정보
     for k in range(len(fireballs)):
@@ -23,12 +24,14 @@ for _ in range(K):
         maps[r][c] -= 1 #기존 위치에서 파이어볼 제거
         maps[x][y] += 1 #이동한 위치에 파이어볼 추가
         newFireballs.append((x,y,m,s,d))
+    #print("newFireballs",newFireballs)
 
     separateFireballs = []  #결과적으로 아래 명령 이후에 존재하는 파이어볼
+    #2이상의 경우    
     for i in range(N):
         for j in range(N):
+            #print("i,j,n",i,j,N)
             if maps[i][j] >= 2:
-
                 #2. 2 이상의 파이어볼이 있는 위치 처리
                 #a. 같은 칸의 파이어볼 합치기
                 #print("here",i,j)
@@ -47,7 +50,7 @@ for _ in range(K):
                         cnt += 1
                 newM = newM//5
                 if newM == 0:  #질량이 0이면 소멸
-                    break
+                    continue
                 newS = newS//cnt
                 #print("new",newM,newS)
                 check = 0
@@ -63,9 +66,11 @@ for _ in range(K):
                     separateFireballs.append((i,j,newM,newS,newD[p]))
             else:
                 for k in range(len(newFireballs)):
+                    #print(i,j,k,newFireballs[k],newFireballs[k][0]==i,newFireballs[k][1]==j)
                     if (newFireballs[k][0] == i and newFireballs[k][1] == j):
                         separateFireballs.append((i,j,newFireballs[k][2],newFireballs[k][3],newFireballs[k][4]))
-    
+
+    #print("separateFireballs",separateFireballs)
     fireballs = separateFireballs 
 
 #print(separateFireballs)
