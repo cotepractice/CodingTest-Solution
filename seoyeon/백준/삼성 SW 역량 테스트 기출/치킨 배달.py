6800d78dcf454047d9fc81ff31e21cd1e9509edb
seoyeon0201 <psy0201123@gmail.com>
2024-02-29 21:39:47 +0900

    [BOJ] 치킨 배달

--- seoyeon/백준/삼성 SW 역량 테스트 기출/치킨 배달.py
+++ seoyeon/백준/삼성 SW 역량 테스트 기출/치킨 배달.py
@@ -1,5 +1,7 @@
 #백준 #15686 치킨 배달
 #삼성 SW 역량 테스트 기출
+#16:13-
+from itertools import combinations
 
 N, M = map(int, input().split())
 Maps = [[0 for _ in range(N)] for _ in range(N)]
@@ -21,51 +23,17 @@ for i in range(N):
             houses.append((i,j))
             houseCnt += 1
 
-allD = [[0 for _ in range(chickenCnt)] for _ in range(houseCnt)]    #집과 치킨집의 모든 거리
-dp = [0 for _ in range(chickenCnt)] #치킨거리에 해당하는 치킨집 카운트 => 많을 수록 M개에 들어갈 확률 높음
-
-for i in range(houseCnt):
-    hx, hy = houses[i]
-    min = 2*N   #치킨거리 
-    idx = []    #치킨거리에 해당하는 인덱스
-    for j in range(chickenCnt):
-        cx, cy = chickens[j]
-        absN = abs(hx-cx) + abs(hy-cy)
-        allD[i][j] = absN
-
-        if absN<min:
-            min = absN
-            idx = [j]
-        elif absN==min:
-            idx.append(j)
-    for k in range(len(idx)):
-        index = idx[k]
-        dp[index] += 1
-
-# print("chicken",chickens)
-# print("house",houses)
-# print("allD",allD)
-# print("dp",*dp)
-
-#M개 선택 => 겹치는 경우 생각 X
-chooseM = [(0,0) for _ in range(chickenCnt)]
-for i in range(chickenCnt):
-    chooseM[i] = (dp[i],i)
-chooseM.sort(reverse=True)  #많은 순서로 정렬
-#print("chooseM",chooseM)
-
-#겹치는 횟수 없는 경우로 가정하고 진행
-index = [0 for _ in range(M)]
-for i in range(M):
-    index[i] = chooseM[i][1]
-# print("index",index)
-
-#선택한 M개의 치킨집 중에서 치킨 거리 계산
-result = 0
-for k in range(houseCnt):
-    d = 2*N
-    for i in index:
-        if allD[k][i] < d:
-            d = allD[k][i]
-    result += d
-print(result)
+#모든 조합 찾아 가장 작은 치킨거리 찾기
+answer = 1000000
+#print(combinations(chickens,M))
+for combi in combinations(chickens,M):  #chickens에 있는 원소들 중 M개 선택. combination
+    #print("combi",combi)
+    sum_distance = 0    #모든 집에서의 치킨거리
+    for house in houses:
+        temp = 1000000  #하나의 집에서의 치킨거리 찾기
+        #집 기준 모든 치킨집과의 조합에서 가장 작은 치킨거리 찾기
+        for x,y in combi:   #모든 조합 중 가장 작은 치킨거리 찾기
+            temp = min(temp, abs(x-house[0])+abs(y-house[1]))   
+        sum_distance += temp
+    answer = min(answer, sum_distance)  #도시의 치킨거리 찾기
+print(answer)
