35abfa2c0b88f39838412324c41df0568ebe0c1f
guyujung <guyujung@naver.com>
2024-04-25 07:37:52 +0900

    Revert "Merge branch 'main' of https://github.com/cotepractice/CodingTest-Solution into 20240420-guyujung"
    This reverts commit d5b3e3f174a3aa8953313a3c17b3a7f0adb66e37, reversing
    changes made to 5a043bd4e33fc4ed1c1b4e51fcd0347e6449c5d0.

--- yoonhye/SQL/GROUP_BY/동명 동물 수 찾기.sql
+++
@@ -1,7 +0,0 @@
--- SQL에서 NULL은 '값이 없음'을 의미하며, IS NULL이나 IS NOT NULL을 통해서만 올바르게 검사할 수 있다.
-SELECT NAME, count(*) AS COUNT
-FROM ANIMAL_INS
-WHERE NAME IS NOT NULL
-GROUP BY NAME
-HAVING count(*) >= 2
-ORDER BY NAME
