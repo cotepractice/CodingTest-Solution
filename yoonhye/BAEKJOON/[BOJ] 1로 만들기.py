b5f20f62863e9464fb7675caa4761f9f413395c0
Yuunhye <pyl66600@gmail.com>
2024-04-30 16:54:20 +0900

    [BOJ] 1로 만들기 - 더 효율적인 방법

--- yoonhye/BAEKJOON/[BOJ] 1로 만들기.py
+++ yoonhye/BAEKJOON/[BOJ] 1로 만들기.py
@@ -3,13 +3,15 @@
 #1을 뺀다.
 
 N = int(input())
-dp = dict()
-dp[1] = 0
-for i in range(1, N):
-    v = dp[i] + 1
-    dp[i*3] = min(v, dp.get(i*3, N))
-    dp[i*2] = min(v, dp.get(i*2, N))
-    dp[i+1] = min(v, dp.get(i+1, N))
+
+dp = [0] * (N+1)
+
+for i in range(2, N+1):
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i//3]+1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2]+1, dp[i])
 
 print(dp[N])
 
