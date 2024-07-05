f9a53409a4863f986d62cc5a7384fe2623bceae2
Yuunhye <pyl66600@gmail.com>
2024-07-05 22:59:57 +0900

    [PGS] 도넛과 막대 그래프 - 1차원 배열만 사용

--- yoonhye/PROGRAMMERS/[PGS] 도넛과 막대 그래프.py
+++ yoonhye/PROGRAMMERS/[PGS] 도넛과 막대 그래프.py
@@ -1,37 +1,22 @@
-# 나가면 +1, 들어오면 -1
-from collections import defaultdict
-
-def find_shape(graph, n):
-    num = n
-    while (1):
-        if len(graph[num]) == 2:
-            return 3
-        elif len(graph[num]) == 0:
-            return 2
-        num = graph[num].pop()
-        if num == n:
-            return 1
-
-
 def solution(edges):
-    answer = [0] * 4
-    graph = defaultdict(list)
-    score = defaultdict(int)
+    N = 1000001
+    outgoing = [0 for _ in range(N)]
+    ingoing = [0 for _ in range(N)]
+    donut, stick, eight = 0, 0, 0
 
     for x, y in edges:
-        graph[x].append(y)
-        score[x] += 1
-        score[y] -= 1
+        outgoing[x] += 1
+        ingoing[y] += 1
 
-    new_node = 0
-    new_node_score = 0
-    for key, value in score.items():
-        if value > new_node_score:
-            new_node = key
-            new_node_score = value
-
-    answer[0] = new_node
-    for n in graph[new_node]:
-        answer[find_shape(graph, n)] += 1
+    for i in range(1, N):
+        if outgoing[i] >= 2:
+            if ingoing[i] == 0:
+                new_node = i
+            else:
+                eight += 1
+        elif outgoing[i] == 0 and ingoing[i] > 0:
+            stick += 1
+    donut = outgoing[new_node] - stick - eight
+    answer = [new_node, donut, stick, eight]
 
     return answer
