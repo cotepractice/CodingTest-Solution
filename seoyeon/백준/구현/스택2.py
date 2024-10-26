1f2bc5a275e0c9bbb6fbe2cfe687f188507034ff
seoyeon0201 <psy0201123@gmail.com>
2024-10-27 00:12:29 +0900

    [BOJ] 스택2

--- seoyeon/백준/구현/스택2.py
+++ seoyeon/백준/구현/스택2.py
@@ -1,8 +1,61 @@
 #백준 #28278 스택2 구현문제
 
-from collections import deque
+#1. 시간초과 발생
+# from collections import deque
 
-global stack
+# global stack
+# stack = deque()
+
+# def one(x):
+#     stack.append(x)
+
+# def two():
+#     if len(stack) != 0:
+#         print(stack.pop())
+#     else:
+#         print(-1)
+
+# def three():
+#     print(len(stack))
+
+# def four():
+#     if len(stack)==0:
+#         print(1)
+#     else:
+#         print(0)
+
+# def five():
+#     if len(stack) != 0:
+#         print(stack.pop())
+#     else:
+#         print(-1)
+
+# N = int(input())
+
+# for i in range(N):
+#     #print("i",i,"stack",stack)
+#     input_lst = list(map(int,input().split()))
+
+#     if input_lst[0] == 1:
+#         one(input_lst[1])
+#         continue
+#     if input_lst[0] == 2:
+#         two()
+#         continue
+#     if input_lst[0] == 3:
+#         three()
+#         continue
+#     if input_lst[0] == 4:
+#         four()
+#         continue
+#     if input_lst[0] == 5:
+#         five()
+#         continue
+
+#2. 
+from collections import deque
+import sys
+
 stack = deque()
 
 def one(x):
@@ -25,15 +78,15 @@ def four():
 
 def five():
     if len(stack) != 0:
-        print(stack.pop())
+        print(stack[-1])
     else:
         print(-1)
 
-N = int(input())
+N = int(sys.stdin.readline())
 
 for i in range(N):
-    #print("i",i,"stack",stack)
-    input_lst = list(map(int,input().split()))
+    #print("stack:",stack)
+    input_lst = list(map(int,sys.stdin.readline().split()))
 
     if input_lst[0] == 1:
         one(input_lst[1])
