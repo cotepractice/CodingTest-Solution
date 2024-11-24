23ce1cd1c6058edfe2792a149a157168000a8c7d
seoyeon0201 <psy0201123@gmail.com>
2024-11-24 14:54:08 +0900

    [BOJ] 최소 힙

---
+++ seoyeon/백준/문제집/IT_기업_및_대기업/최소힙.py
@@ -0,0 +1,23 @@
+#백준 #1927 최소 힙
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+heap = []
+
+N = int(input())
+
+for i in range(N):
+    x = int(input())
+
+    #1.x가 자연수인 경우 배열에 X 추가
+    if x>0:
+        heapq.heappush(heap, x)
+    #2.x가 0인 경우 배열에서 가장 작은 값 출력 후 배열에서 해당 값 제거
+    if x==0:
+        if len(heap)==0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+
