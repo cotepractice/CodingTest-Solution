13a4409ed61bd1c4b5f74d3c1fddd894c94d0167
seoyeon0201 <psy0201123@gmail.com>
2025-01-10 21:59:26 +0900

    [BOJ] 16926

--- seoyeon/백준/SAFFY_Rec/16926.py
+++ seoyeon/백준/SAFFY_Rec/16926.py
@@ -1,69 +1,34 @@
-#백준 #16926 배열 돌리기 1
+import sys
+from collections import deque
 
-N, M, R = map(int, input().split())
+N, M, R = map(int, sys.stdin.readline().split())
 
-maps = [[0 for _ in range(M+1)] for _ in range(N+1)]
+table = [sys.stdin.readline().split() for _ in range(N)]
+final = [[0] * M for _ in range(N)]  # 빈 이차원 배열 생성 N * M 사이즈
+deq_box = deque()  # 빈 데크 박스 생성
 
-for i in range(1,N+1):
-    lst_i = [0]
-    lst = list(map(int,input().split()))
-    for k in range(M):
-        lst_i.append(lst[k])
-    maps[i] = lst_i
+# 몇 겺으로 되어 있는지 -> 결국 몇 번 루프를 돌아야 하는지
+loops = min(N, M) // 2  # 가장 작은 수의 절반 만큼 겹이 쌓임
+for i in range(loops):
+    deq_box.clear()
+    # 돌리기 전의 1차원 deq 생성 - 위 오른쪽 아래 왼쪽 순서로 넣어준다
+    deq_box.extend(table[i][i : M - i])  # 위
+    deq_box.extend([row[M - i - 1] for row in table[i + 1 : N - i - 1]])  # 오
+    deq_box.extend(table[N - i - 1][i : M - i][::-1])  # 아래
+    deq_box.extend([row[i] for row in table[i + 1 : N - i - 1]][::-1])  # 왼
 
-def left(x,y):
-    return x,y-1
+    deq_box.rotate(-R)
 
-def right(x,y):
-    return x,y+1
+    # 생성된 deq_box 의 맨 처음 거를 빼서 final 에 하나씩 담는다 - 위 오른쪽 아래 왼쪽 순서로
+    # i 가 0 이면..
+    for j in range(i, M - i):  # 위 (0,0)(0,1)(0,M)
+        final[i][j] = deq_box.popleft()
+    for j in range(i + 1, N - i - 1):  # 오른쪽 (1, 오른쪽 맨 끝)
+        final[j][M - i - 1] = deq_box.popleft()
+    for j in range(M - i - 1, i - 1, -1):  # 아래 (오른쪽 맨 아래,,)
+        final[N - i - 1][j] = deq_box.popleft()
+    for j in range(N - i - 2, i, -1):
+        final[j][i] = deq_box.popleft()
 
-def up(x,y):
-    return x-1,y
-
-def down(x,y):
-    return x+1,y
-
-def move(x,y): #depth는 1부터 min(N,M)//2까지 동작, N과 M은 1부터 시작. x와 y는 1부터 시작
-
-        d = min(x,y,N-x+1,M-y+1)
-
-        #up
-        if d+1<=x<=N-d+1 and y == M-d+1:
-            #print("1",up(x,y))
-            return up(x,y)
-            
-        #down
-        if d<=x<=N-d and y == d:
-            #print("2",down(x,y))
-            return down(x,y)
-        #right
-        if d<=y<=M-d and x == N-d+1:
-            #print("3",right(x,y))
-            return right(x,y)
-
-        #left
-        if d+1<=y<=M-d+1 and x == d:
-            #print("4",left(x,y))
-            return left(x,y)
-
-
-
-move_maps = [[0 for _ in range(M+1)] for _ in range(N+1)]
-
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        cur_val = maps[i][j]
-        cur_x,cur_y = i,j
-        #print("cur",cur_x,cur_y)
-        #rotate_N = min(N,M)//2 #문제에 작은게 짝수라고 명시되어 있음
-        for r in range(R):
-            mv_x, mv_y = move(cur_x,cur_y)
-            cur_x,cur_y = mv_x,mv_y
-        #print("mv",mv_x,mv_y)
-        move_maps[mv_x][mv_y] = cur_val
-
-#print(move_maps)
-for i in range(1,N+1):
-    for j in range(1,M+1):
-        print(move_maps[i][j], end=" ")
-    print(end="\n")
+for i in final:
+    print(*i)
