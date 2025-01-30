ae589efbe0263aa305b9c32b990aeade360edde9
seoyeon0201 <psy0201123@gmail.com>
2025-01-30 19:09:13 +0900

    [BOJ] 1987

--- seoyeon/백준/SAFFY_Rec/1987.py
+++ seoyeon/백준/SAFFY_Rec/1987.py
@@ -1,38 +1,39 @@
 #백준 #1987 알파벳
 
-def dfs(x,y,alpha,cnt):
-    global ans
+def dfs(x, y):
+    global board, R, C
 
-    if cnt>ans:
-        ans = cnt
+    movement_array = [[-1,0],[1,0],[0,-1],[0,1]]
 
-    d_lst = [[0,-1],[0,1],[1,0],[-1,0]]
+    max_depth = 0
+    queue = set() # 중복 방지. 동일한 위치 오지 않도록 함
+    queue.add((x, y, board[x][y]))
+    while queue: 
+        current_x, current_y, current_visited = queue.pop() #current_visited는 문자열
+        
+        max_depth = max(max_depth, len(current_visited))
+        if max_depth == 26: #알파벳 개수
+            return 26
+        for movement in movement_array:
+            next_x = current_x + movement[0]
+            next_y = current_y + movement[1]
+            if 0 <= next_x < R and 0 <= next_y < C and board[next_x][next_y] not in current_visited:
+                queue.add((next_x, next_y, current_visited + board[next_x][next_y]))
+    return max_depth       
 
-    for dx,dy in d_lst:
-        nx = x+dx
-        ny = y+dy
-        if 0<=nx<R and 0<=ny<C and visited[nx][ny]==False and boards[nx][ny] not in alpha:
-            visited[nx][ny]=True
-            alpha[boards[nx][ny]]=0
-            dfs(nx,ny,alpha,cnt+1)
-            visited[nx][ny]=False
-            del alpha[boards[nx][ny]]
-            
 
 
 R, C = map(int,input().split())
-boards = ["" for _ in range(R)]
+board = ["" for _ in range(R)]
 
 for r in range(R):
-    lst = input()
-    boards[r] = lst
+    lst=input()
+    board[r]=lst
 
-ans = 0
 visited=[[False for _ in range(C)] for _ in range(R)]
 
 visited[0][0]=True
-dict = {}
-dict[boards[0][0]]=0
-dfs(0,0,dict,1)
 
-print(ans)
+print(dfs(0,0))
+
+#print(max_depth)
