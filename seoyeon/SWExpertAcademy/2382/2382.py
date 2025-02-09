a318fd2b532e65e4bf7c37ee2a1e3e00bd0d9553
seoyeon0201 <psy0201123@gmail.com>
2025-02-09 20:29:32 +0900

    [SWEA] 2382 미생물 격리

--- seoyeon/SWExpertAcademy/2382/2382.py
+++ seoyeon/SWExpertAcademy/2382/2382.py
@@ -1,96 +1,65 @@
 #SWExpert Academy #2382 미생물 격리
 
-import sys
-sys.stdin = open("input.txt", "r")
-
-import copy
-
-def up(x,y,num,d,cp_board):
-    global visited 
-    cp_board[x][y] = [0,0]
-    x -= 1
-
-    #방향 변경&미생물 수 변경
-    if x==0 or x==N-1 or y==0 or y==N-1:
-        d = 2 #방향 아래로 변경
-        cp_board[x][y] = [num//2,d]
-    else:
-        cp_board[x][y] = [num,d]
-    return cp_board
-    
-def down(x,y,num,d,cp_board):
-    cp_board[x][y]=[0,0]
-    x += 1
-
-    #방향 변경&미생물 수 변경
-    if x==0 or x==N-1 or y==0 or y==N-1:
-        d = 1 #방향 위로 변경
-        cp_board[x][y] = [num//2,d]
-    else:
-        cp_board[x][y] = [num,d]
-    return cp_board
-
-def left(x,y,num,d,cp_board):
-    cp_board[x][y]=[0,0]
-    y -= 1
-
-    #방향 변경&미생물 수 변경
-    if x==0 or x==N-1 or y==0 or y==N-1:
-        d = 4 #방향 위로 변경
-        cp_board[x][y] = [num//2,d]
-    else:
-        cp_board[x][y] = [num,d]
-    return cp_board    
-
-def right(x,y,num,d,cp_board):
-    cp_board[x][y]=[0,0]
-    y += 1
-
-    #방향 변경&미생물 수 변경
-    if x==0 or x==N-1 or y==0 or y==N-1:
-        d = 3 #방향 위로 변경
-        cp_board[x][y] = [num//2,d]
-    else:
-        cp_board[x][y] = [num,d]
-    return cp_board    
-
-def move():
-    cp_board = copy.deepcopy(board)
-    #print(cp_board)
-    visited=[[False for _ in range(N)] for _ in range(N)]
-
-    for i in range(N):
-        for j in range(N):
-            num,dir = board[i][j] #미생물 수, 이동방향
-            if x==-1:
-                continue
-            if dir==1:
-                cp_board = up(i,j,num,dir,cp_board)
-            elif dir==2:
-                cp_board = down(i,j,num,dir,cp_board)
-            elif dir==3:
-                cp_board = left(i,j,num,dir,cp_board)
-            elif dir==4:
-                cp_board = right(i,j,num,dir,cp_board)
-    return cp_board
-
 T = int(input())
-for t in range(1,T+1):
-    N,M,K = map(int,input().split())
-    board = [[[0,0] for _ in range(N)] for _ in range(N)]
+for test_case in range(1, T+1):
+    N, M, K = map(int, input().split())
+    microbes = [list(map(int, input().split())) for _ in range(K)]
 
-    for m in range(K):
-        x,y,n,d = list(map(int,input().split()))
-        board[x][y] = [n,d]
-    
-    for _ in range(M):
-        board = move()
-        #print(board)
-        
+    answer = 0
 
-    ans = 0
-    for i in range(N):
-        for j in range(N):
-            ans += board[i][j][0]
-    print("#",t,sep="",end=" ")
-    print(ans)
+    for time in range(M):
+        merge = []
+        for microbe in range(len(microbes)):
+
+            if microbes[microbe][3] == 1:
+                microbes[microbe][0] -= 1
+            elif microbes[microbe][3] == 2:
+                microbes[microbe][0] += 1
+            elif microbes[microbe][3] == 3:
+                microbes[microbe][1] -= 1
+            elif microbes[microbe][3] == 4:
+                microbes[microbe][1] += 1
+
+            if microbes[microbe][0] == 0 or microbes[microbe][0] == N-1 or microbes[microbe][1] == 0 or microbes[microbe][1] == N-1:
+                microbes[microbe][2] = microbes[microbe][2] // 2
+
+                if microbes[microbe][3] == 1:
+                    microbes[microbe][3] = 2
+                elif microbes[microbe][3] == 2:
+                    microbes[microbe][3] = 1
+                elif microbes[microbe][3] == 3:
+                    microbes[microbe][3] = 4
+                elif microbes[microbe][3] == 4:
+                    microbes[microbe][3] = 3
+
+            location = [microbes[microbe][0], microbes[microbe][1]] 
+
+            if location not in merge:
+                merge.append(location)
+
+        for i in merge:  
+            microbes_big = 0  
+            microbes_num = 0 
+            microbes_sum = 0  
+            temp_list = []
+
+            for j in range(len(microbes)-1, -1, -1): 
+
+                if [microbes[j][0], microbes[j][1]] == i:  
+                    if microbes[j][2] > microbes_big: 
+                        microbes_big = microbes[j][2] 
+                        microbes_num = j
+                    temp_list.append(j)
+
+            dir = microbes[microbes_num][3]
+
+            for k in temp_list:
+                pop_micro = microbes.pop(k)
+                microbes_sum += pop_micro[2]
+
+            microbes.append([i[0], i[1], microbes_sum, dir])
+
+    for microbe in microbes:
+        answer += microbe[2]
+
+    print(f'#{test_case} {answer}')
