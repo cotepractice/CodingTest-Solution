29e7002fec3f4d019a5ae18873068c8e3cd2427f
seoyeon <psy0201123@gmail.com>
2025-05-01 16:33:27 +0900

    [BOJ] 11726 2xn 타일링

--- seoyeon/백준/DP/11726.py
+++ seoyeon/백준/DP/11726.py
@@ -4,11 +4,11 @@
 #2*N 크기의 직사각형을 채우는 방법의 수 
 N = int(input())
 
-dp = [0 for _ in range(N+1)]
+dp = [0 for _ in range(10001)]
 
 dp[1]=1
 dp[2]=2
 
-for i in range(3,N+1):
+for i in range(3,10001):
     dp[i] = dp[i-1]+dp[i-2]
 print((dp[N])%10007)
